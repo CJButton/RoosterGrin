33d0557e516aa905812f064377dfe07e8ec2a886
CJButton <gsyme1907@gmail.com>
2017-04-02 00:47:15 -0700

    modals created, main page 95% done

--- frontend/components/main/main.jsx
+++ frontend/components/main/main.jsx
@@ -22,16 +22,27 @@ class Main extends React.Component{
       email: "",
       formatHide: "",
       formatShow: "formatHide",
-      finalText: []
+      finalText: [],
+      modal: false
     };
   }
 
   componentWillReceiveProps(nextProps) {
+
+    let properPhone = [];
+    let areaCode;
+    if(nextProps.patientInfo.phone_number !== undefined) {
+      areaCode = nextProps.patientInfo.phone_number.slice(0, 3);
+      properPhone.push(`(${areaCode})`)
+      properPhone.push(`${nextProps.patientInfo.phone_number.slice(3, 6)}-`);
+      properPhone.push(`${nextProps.patientInfo.phone_number.slice(6)}`);
+    }
+
     this.setState({
       fname: nextProps.patientInfo.first_name,
       lname: nextProps.patientInfo.last_name,
       street: nextProps.patientInfo.street_address,
-      phone: nextProps.patientInfo.phone_number,
+      phone: properPhone.join(""),
       city: nextProps.patientInfo.city,
       state: nextProps.patientInfo.state,
       zip: nextProps.patientInfo.zip,
@@ -45,17 +56,28 @@ class Main extends React.Component{
     });
   }
 
+  componentWillMount() {
+    Modal.setAppElement('body');
+   }
+
+  submitHit() {
+    if (this.state.currentPatient === "Select a Patient") {
+      this.setState({
+        modal: true
+      })
+    } else {
+      this.handleSubmit();
+    }
+  }
+
+  closeModal() {
+    this.setState({
+      modal: false
+    });
+  }
+
   handleSubmit() {
     let newLine = this.state.text.split("\n");
-    // let info = ["@fname", "@lname", "@street", "@phone",
-    //             "@city", "@state", "@zip","@email"]
-    // let test = "@lname"
-    // test.replace("@fname", "string replacement");
-    // console.log("@fname".replace("@", "test"));
-    // this.state.finalText.push("@lname".replace("@lname", this.state.lname));
-    // console.log(test);
-
-
 
     newLine.forEach((span) => {
       let splitSpan = span.split(" ");
@@ -85,8 +107,6 @@ class Main extends React.Component{
       this.state.finalText.push("\n");
     });
 
-
-    console.log(this.state.finalText);
     this.setState({
       formatHide: "formatHide",
       formatShow: ""
@@ -102,29 +122,45 @@ class Main extends React.Component{
 
 
   render() {
-    console.log(this.state);
     return (
     <div className="mainWrapper">
 
       <div className={`formatWrapper` + " " + this.state.formatHide}>
-        <h1>Email formatting:</h1>
-        <Dropdown options={this.props.patients}
-          onChange={this.selectPatient.bind(this)}
-          placeholder={this.state.currentPatient} />
-        <br></br>
-        <br></br>
-        <div className="textAreaContainer">
-          <p>What would you like to write?</p>
-          <textarea className="textArea"
-            onChange={this.handleText.bind(this)}
-            value={this.state.text}/>
-          <br></br>
-          <button className="submitButton"
-            onClick={this.handleSubmit.bind(this)}>Submit!</button>
-        </div>
 
+        <li className="instructions"> When writing your note, please be aware of the following keys:
+          <ul>@fname: Patient's first name</ul>
+          <ul>@lname:  Patient's last name</ul>
+          <ul>@street: 12345 ABC Street</ul>
+          <ul>@phone:  (123)456-789</ul>
+          <ul>@city:   San Francisco</ul>
+          <ul>@state:  California</ul>
+          <ul>@zip:   123456</ul>
+          <ul>@email:  sample@email.com</ul>
+        </li>
+
+          <p className="dropdownInstruction">Please choose a patient from the dropdown:</p>
+          <Dropdown options={this.props.patients}
+                    onChange={this.selectPatient.bind(this)}
+                    placeholder={this.state.currentPatient} />
+          <br></br>
+
+          <div className="textAreaFlex">
+
+            <div className="textAreaContainer">
+              <p className="textAreaTitle">What would you like to write?</p>
+              <textarea className="textArea"
+                        onChange={this.handleText.bind(this)}
+                        value={this.state.text}/>
+              <br/>
+              <button className="submitButton button"
+                      onClick={this.submitHit.bind(this)}>Submit!</button>
+            </div>
+          </div>
+          <br/>
+          <br/>
 
       </div>
+
 
       <div className={this.state.formatShow}>
           {this.state.finalText.map((word, idx) => {
@@ -139,26 +175,27 @@ class Main extends React.Component{
           })}
       </div>
 
-    </div>
 
+      <Modal className="warningModal"
+             isOpen={this.state.modal}>
+
+          <p className="modalTitle">
+              Are you sure you want to submit without choosing a patient?</p>
+
+            <div className="submitChecker">
+            <button className="closeModal button"
+                    onClick={this.closeModal.bind(this)}>
+                    No! Go back!</button>
+
+            <button className="proceedSubmit button"
+                    onClick={this.handleSubmit.bind(this)}>
+                    Yes! Proceed!</button>
+            </div>
+        </Modal>
+
+    </div>
     );
   }
 }
 
-// {this.state.text.split("\n").map((sentence, idx) => {
-//   return(
-//     // <span key={idx}>{sentence}<br/></span>
-//     <span key={idx}>{this.state[sentence]}</span>
-//   );
-// })}
 export default Main;
-// return (React.createElement(item.element, null, item.text))
-
-// if (word.includes("@")) {
-//   let stateWord = word.split("@")[1]
-//   return <span key={idx}>{this.state[stateWord]}</span>
-// } else if (word === "\n"){
-//   return "\n"
-// } else {
-//   return <span key={idx}>{word}</span>
-// }
